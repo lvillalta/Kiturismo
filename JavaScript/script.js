baafbc5612e0d3d5cd460c72f9618396ad5dcf42
lvillalta <luis.villalta@epn.edu.ec>
2017-12-10 01:13:58 -0500

    formulario

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -18,3 +18,4 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
---
+++ JavaScript/scriptC.js
@@ -0,0 +1,25 @@
+window.onload =function () {
+   //variables
+    var form1,nombre,email,asunto,mensaje,pos;
+    var mens="";
+    //Instancias
+    form1= document.getElementById("form1");
+    nombre= document.getElementById("nombre");
+    email= document.getElementById("email");
+    asunto= document.getElementById("asunto");
+    mensaje= document.getElementById("mensaje");
+    
+    form1.onsubmit=function(){
+        if(nombre.value.length==0)
+          
+          mens+="Ingrese un nombre";
+        }
+        if(email.value.length==0){
+            
+            mens+="Ingrese un correo";
+        }
+        if(mens.length!=0){
+            alert(mens);
+        }
+
+}
