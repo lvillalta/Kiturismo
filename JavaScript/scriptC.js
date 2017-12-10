49222690ad9f754b810c408c94e90c0bc31aedcd
lvillalta <luis.villalta@epn.edu.ec>
2017-12-10 16:55:12 -0500

    validación formulario

--- JavaScript/scriptC.js
+++ JavaScript/scriptC.js
@@ -17,10 +17,77 @@ window.onload =function () {
         if(email.value.length==0){
             
             mens+="\nIngrese un correo";
+        }else{
+            if(email.value.length<10){
+               mens+="\nEl email ingresado es muy corto";
+                }else{
+                    pos=email.value.indexOf("@");
+                    if(pos==-1){
+                        mens+="\nemail ingresado no válido, no tiene arroba";
+                    }else{
+                        pos=email.value.indexOf(".");
+                        if(pos==-1){
+                            mens+="\nemail ingresado no válido, no tiene punto";
+                        }
+                    }
+            }
+
         }
         if(mens.length!=0){
             alert(mens);
+        }else{
+                alert(nombre.value+"\n"+
+                email.value+"\n"+
+                asunto.value +"\n"+
+                mensaje.value
+                
+                );
         }
     }
 
+
+    //Creamos los Listeners
+    //nombre
+    nombre.onfocus=function(){
+        enciende(nombre);
+    }
+    nombre.onblur=function(){
+        apaga(nombre);
+    }
+    
+    //email
+    email.onfocus=function(){
+        enciende(email);
+    }
+    email.onblur=function(){
+        apaga(email);
+    }
+
+    //asunto
+    asunto.onfocus=function(){
+        enciende(asunto);
+    }
+    asunto.onblur=function(){
+        apaga(asunto);
+    }
+
+    //mensaje
+    mensaje.onfocus=function(){
+        mensaje.value="";
+        enciende(mensaje);
+    }
+    mensaje.onblur=function(){
+        apaga(mensaje);
+    }
+
+
+}
+
+//funciones
+function enciende (campo) {
+    campo.setAttribute("class","enciende");
+}
+
+function apaga (campo) {
+    campo.setAttribute("class","apaga");
 }
